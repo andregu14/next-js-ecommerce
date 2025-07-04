1443c32963d139715369343b2ccbeed5592ff97a
André Gustavo <andregustavo147@gmail.com>
2025-07-04 02:00:03 -0300

    feat: create email PurchaseReceipt email

---
+++ src/email/PurchaseReceipt.tsx
@@ -0,0 +1,62 @@
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Preview,
+  Tailwind,
+} from "@react-email/components";
+import { OrderInformation } from "./components/OrderInformation";
+
+type PurchaseReceiptEmailProps = {
+  product: {
+    name: string;
+    imagePath: string;
+    description: string;
+  };
+  order: { id: string; createdAt: Date; pricePaidInCents: number };
+  downloadVerificationId: string; 
+};
+
+PurchaseReceiptEmail.PreviewProps = {
+  product: {
+    name: "Nome do Produto",
+    imagePath:
+      "/products/82a64689-c21c-4c03-a036-c7ba3aadd424-secure-wifi-without-changing-password.png",
+    description: "teste",
+  },
+  order: {
+    id: crypto.randomUUID(),
+    createdAt: new Date(),
+    pricePaidInCents: 10000,
+  },
+  downloadVerificationId: crypto.randomUUID(),
+} satisfies PurchaseReceiptEmailProps;
+
+export default function PurchaseReceiptEmail({
+  product,
+  order,
+  downloadVerificationId,
+}: PurchaseReceiptEmailProps) {
+  return (
+    <Html>
+      <Preview>
+        Download {product.name} e veja o comprovante de pagamento
+      </Preview>
+      <Tailwind>
+        <Head />
+        <Body className="font-sans bg-white">
+          <Container className="max-w-xl">
+            <Heading>Comprovante de Pagamento</Heading>
+            <OrderInformation
+              order={order}
+              product={product}
+              downloadVerificationId={downloadVerificationId}
+            />
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+}
