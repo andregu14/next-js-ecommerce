f2ef7bd5e18161a9be49dca8438a2c22f8cd9606
André Gustavo <andregustavo147@gmail.com>
2025-07-04 02:01:13 -0300

    feat: create ordersPage

---
+++ src/app/admin/pedidos/page.tsx
@@ -0,0 +1,82 @@
+import { PageHeader } from "../_components/PageHeader";
+import { DeleteDropDownItem } from "../../admin/pedidos/_components/OrderActions";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import db from "@/lib/db";
+import { MoreVertical } from "lucide-react";
+import { formatCurrency } from "@/lib/formatters";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+function getOrders() {
+  return db.order.findMany({
+    select: {
+      id: true,
+      pricePaidInCents: true,
+      product: { select: { name: true } },
+      user: { select: { email: true } },
+    },
+    orderBy: { createdAt: "desc" },
+  });
+}
+
+export default function OrdersPage() {
+  return (
+    <>
+      <PageHeader>Vendas</PageHeader>
+      <OrdersTable />
+    </>
+  );
+}
+
+async function OrdersTable() {
+  const orders = await getOrders();
+
+  if (orders.length === 0) return <p>Nenhum venda registrada</p>;
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Produto</TableHead>
+          <TableHead>Cliente</TableHead>
+          <TableHead>Preço Pago</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Ações</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {orders.map((order) => (
+          <TableRow key={order.id}>
+            <TableCell>{order.product.name}</TableCell>
+            <TableCell>{order.user.email}</TableCell>
+            <TableCell>
+              {formatCurrency(order.pricePaidInCents / 100)}
+            </TableCell>
+            <TableCell className="text-center">
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <MoreVertical className="cursor-pointer" />
+                  <span className="sr-only">Ações</span>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DeleteDropDownItem id={order.id} />
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
