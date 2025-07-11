c0c53c47e977d4a003c581c518055d1758fc983f
André Gustavo <andregustavo147@gmail.com>
2025-07-10 22:42:57 -0300

    chore: change cards layout

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,12 +1,17 @@
+import { TrendingUp, TrendingDown } from "lucide-react";
 import {
   Card,
+  CardAction,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import db from "@/lib/db";
 import { formatCurrency, formatNumber } from "@/lib/formatters";
+import { AdminHeader } from "./_components/ui/admin-header";
+import { Badge } from "@/components/ui/badge";
 
 async function getSalesData() {
   const data = await db.order.aggregate({
@@ -45,34 +50,94 @@ async function getProductData() {
   return {
     activeCount,
     inactiveCount,
-  }
+  };
 }
 
 export default async function AdminDashboard() {
   const [salesData, userData, productData] = await Promise.all([
     getSalesData(),
     getUserData(),
-    getProductData()
+    getProductData(),
   ]);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      <DashboardCard
-        title="Vendas"
-        subtitle={`${formatNumber(salesData.numberOfSales)} Pedidos`}
-        body={formatCurrency(salesData.amount)}
-      />
-      <DashboardCard
-        title="Clientes"
-        subtitle={`${formatCurrency(userData.averegaValuePerUser)} Valor médio`}
-        body={formatNumber(userData.userCount)}
-      />
-      <DashboardCard
-        title="Produtos à Venda"
-        subtitle={`${formatNumber(productData.inactiveCount)} Inativos`}
-        body={formatNumber(productData.activeCount)}
-      />
-    </div>
+    <>
+      <AdminHeader currentPage="Dashboard" />
+      <div className="flex flex-1 flex-col">
+        <div className="@container/main flex flex-1 flex-col gap-2">
+          <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
+              <Card className="@container/card">
+                <CardHeader>
+                  <CardDescription>Vendas</CardDescription>
+                  <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                    {formatCurrency(salesData.amount)}
+                  </CardTitle>
+                  <CardAction>
+                    <Badge variant="outline">
+                      <TrendingUp />
+                      +12.5%
+                    </Badge>
+                  </CardAction>
+                </CardHeader>
+                <CardFooter className="flex-col items-start gap-1.5 text-sm">
+                  <div className="line-clamp-1 flex gap-2 font-medium">
+                    {`${formatNumber(salesData.numberOfSales)} Novos Pedidos`}{" "}
+                    <TrendingUp className="size-4" />
+                  </div>
+                  <div className="text-muted-foreground">
+                    Vendas nos últimos 6 meses
+                  </div>
+                </CardFooter>
+              </Card>
+              <Card className="@container/card">
+                <CardHeader>
+                  <CardDescription>Clientes</CardDescription>
+                  <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                    {formatNumber(userData.userCount)}
+                  </CardTitle>
+                  <CardAction>
+                    <Badge variant="outline">
+                      <TrendingDown />
+                      -20%
+                    </Badge>
+                  </CardAction>
+                </CardHeader>
+                <CardFooter className="flex-col items-start gap-1.5 text-sm">
+                  <div className="line-clamp-1 flex gap-2 font-medium">
+                    {`${formatCurrency(userData.averegaValuePerUser)} Valor Médio Gasto`}{" "}
+                    <TrendingDown className="size-4" />
+                  </div>
+                  <div className="text-muted-foreground">Total de clientes</div>
+                </CardFooter>
+              </Card>
+              <Card className="@container/card">
+                <CardHeader>
+                  <CardDescription>Produtos à Venda</CardDescription>
+                  <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                    {formatNumber(productData.activeCount)}
+                  </CardTitle>
+                  <CardAction>
+                    <Badge variant="outline">
+                      <TrendingUp />
+                      +12.5%
+                    </Badge>
+                  </CardAction>
+                </CardHeader>
+                <CardFooter className="flex-col items-start gap-1.5 text-sm">
+                  <div className="line-clamp-1 flex gap-2 font-medium">
+                    Aumento do número de itens <TrendingUp className="size-4" />
+                  </div>
+                  <div className="text-muted-foreground">
+                    {`${formatNumber(productData.inactiveCount)} Inativos`}
+                  </div>
+                </CardFooter>
+              </Card>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
