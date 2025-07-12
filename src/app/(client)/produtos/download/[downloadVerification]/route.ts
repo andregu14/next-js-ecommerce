65f65b6a45cc37bb03ad9cdf3f6fe7673298619b
André Gustavo <andregustavo147@gmail.com>
2025-07-11 23:21:52 -0300

    fix: await params

--- src/app/(client)/produtos/download/[downloadVerification]/route.ts
+++ src/app/(client)/produtos/download/[downloadVerification]/route.ts
@@ -4,10 +4,12 @@ import fs from "fs/promises";
 
 export async function GET(
   req: NextRequest,
-  {
-    params: { downloadVerification }, 
-  }: { params: { downloadVerification: string } }
+  context: { params: { downloadVerification: string } }
 ) {
+  const { params } = context;
+  const awaitedParams = await params;
+  const downloadVerification = awaitedParams.downloadVerification;
+
   const data = await db.downloadVerification.findUnique({
     where: { id: downloadVerification, expiresAt: { gt: new Date() } },
     select: { product: { select: { filePath: true, name: true } } },
@@ -20,13 +22,13 @@ export async function GET(
   }
 
   const { size } = await fs.stat(data.product.filePath);
-    const file = await fs.readFile(data.product.filePath);
-    const extension = data.product.filePath.split(".").pop();
-  
-    return new NextResponse(file, {
-      headers: {
-        "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
-        "Content-Length": size.toString(),
-      },
-    });
+  const file = await fs.readFile(data.product.filePath);
+  const extension = data.product.filePath.split(".").pop();
+
+  return new NextResponse(file, {
+    headers: {
+      "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
+      "Content-Length": size.toString(),
+    },
+  });
 }
