55113fd0c7ce2b14341e741b3c5281b85667a5e1
André Gustavo <andregustavo147@gmail.com>
2025-07-14 21:58:52 -0300

    chore: update getUserData function

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,6 +3,7 @@ import { AdminHeader } from "./_components/ui/admin-header";
 import { SalesChart } from "./_components/SalesChart";
 import { DashboardCardGroup } from "./_components/DashboardCardGroup";
 import { subMonths, startOfMonth } from "date-fns";
+import { UsersChart } from "./_components/UsersChart";
 
 async function getSalesData() {
   const now = new Date();
@@ -56,6 +57,38 @@ async function getSalesData() {
 }
 
 async function getUserData() {
+  const now = new Date();
+  const startDate = startOfMonth(subMonths(now, 5));
+
+  const usersByMonth = await db.user.groupBy({
+    by: ["createdAt"],
+    _count: true,
+    where: {
+      createdAt: {
+        gte: startDate,
+      },
+    },
+  });
+
+  const chartData = [];
+  for (let i = 0; i < 6; i++) {
+    const date = subMonths(now, 5 - i);
+    const month = date.toLocaleString("pt-BR", { month: "long" });
+    const year = date.getFullYear();
+    // Encontrar numero de usuario desse mes
+    const clientes = usersByMonth
+      .filter((item) => {
+        const d = new Date(item.createdAt);
+        return d.getMonth() === date.getMonth() && d.getFullYear() === year;
+      })
+      .reduce((acc, item) => acc + (item._count ?? 0), 0);
+
+    chartData.push({
+      month: month.charAt(0).toUpperCase() + month.slice(1),
+      clientes,
+    });
+  }
+
   const [userCount, orderData] = await Promise.all([
     db.user.count(),
     db.order.aggregate({
@@ -69,6 +102,7 @@ async function getUserData() {
       userCount === 0
         ? 0
         : (orderData._sum.pricePaidInCents || 0) / userCount / 100,
+    chartData,
   };
 }
 
@@ -96,14 +130,15 @@ export default async function AdminDashboard() {
       <div className="flex flex-1 flex-col">
         <div className="@container/main flex flex-1 flex-col gap-2">
           <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-            <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-3">
-              <DashboardCardGroup
-                salesData={salesData}
-                userData={userData}
-                productData={productData}
-              />
-              <SalesChart chartData={salesData.chartData} />
-            </div>
+            <DashboardCardGroup
+              salesData={salesData}
+              userData={userData}
+              productData={productData}
+            />
+          </div>
+          <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @5xl/main:grid-cols-2 mb-8 ">
+            <SalesChart chartData={salesData.chartData} />
+            <UsersChart chartData={userData.chartData} />
           </div>
         </div>
       </div>
