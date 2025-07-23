ddfaac6e6d4977ad929a4c59d6094324549631fb
André Gustavo <andregustavo147@gmail.com>
2025-07-23 18:34:52 -0300

    feat: use dashboard data table component

--- src/app/admin/produtos/page.tsx
+++ src/app/admin/produtos/page.tsx
@@ -1,51 +1,14 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 import db from "@/lib/db";
-import { CheckCircle2, MoreVertical, XCircle } from "lucide-react";
-import { formatCurrency, formatNumber } from "@/lib/formatters";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import {
-  ActiveToggleDropdownItem,
-  DeleteDropdownItem,
-} from "./_components/ProductActions";
 import { AdminHeader } from "../_components/ui/admin-header";
+import { DashboardDataTable } from "../_components/DashboardDataTable";
 
-export default function AdminProductsPage() {
-  return (
-    <>
-      <AdminHeader
-        previousPage={[{ title: "Dashboard", url: "/admin" }]}
-        currentPage="Produtos"
-      />
-      <div className="flex justify-between items-center gap4'">
-        <Button asChild>
-          <Link href={"/admin/produtos/novo"}>Adicionar Produto</Link>
-        </Button>
-      </div>
-      <ProductsTable />
-    </>
-  );
-}
-
-async function ProductsTable() {
+export default async function AdminProductsPage() {
   const products = await db.product.findMany({
     select: {
       id: true,
       name: true,
+      imagePath: true,
+      description: true,
       priceInCents: true,
       isAvailableForPurchase: true,
       _count: { select: { orders: true } },
@@ -60,71 +23,12 @@ async function ProductsTable() {
   }
 
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-0">
-            <span className="sr-only">Disponível Para Compra</span>
-          </TableHead>
-          <TableHead>Nome</TableHead>
-          <TableHead>Preço</TableHead>
-          <TableHead>Pedidos</TableHead>
-          <TableHead className="w-0">
-            <span className="sr-only">Ações</span>
-          </TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {products.map((product) => (
-          <TableRow key={product.id}>
-            <TableCell>
-              {product.isAvailableForPurchase ? (
-                <>
-                  <CheckCircle2 />
-                  <span className="sr-only">Disponível</span>
-                </>
-              ) : (
-                <>
-                  <XCircle className="stroke-destructive" />
-                  <span className="sr-only">Indisponível</span>
-                </>
-              )}
-            </TableCell>
-            <TableCell>{product.name}</TableCell>
-            <TableCell>{formatCurrency(product.priceInCents / 100)}</TableCell>
-            <TableCell>{formatNumber(product._count.orders)}</TableCell>
-            <TableCell>
-              <DropdownMenu>
-                <DropdownMenuTrigger>
-                  <MoreVertical className="cursor-pointer" />
-                  <span className="sr-only">Ações</span>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  <DropdownMenuItem asChild>
-                    <a download href={`/admin/produtos/${product.id}/download`}>
-                      Download
-                    </a>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem asChild>
-                    <Link href={`/admin/produtos/${product.id}/editar`}>
-                      Editar
-                    </Link>
-                  </DropdownMenuItem>
-                  <ActiveToggleDropdownItem
-                    id={product.id}
-                    isAvailableForPurchase={product.isAvailableForPurchase}
-                  />
-                  <DropdownMenuSeparator />
-                  <DeleteDropdownItem
-                    id={product.id}
-                    disabled={product._count.orders > 0}
-                  />
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <>
+      <AdminHeader
+        previousPage={[{ title: "Dashboard", url: "/admin" }]}
+        currentPage="Produtos"
+      />
+      <DashboardDataTable salesData={products} dataType="sales" />
+    </>
   );
 }
