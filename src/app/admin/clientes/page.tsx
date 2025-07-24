be5bb617bef98e46b27990b9c51c456ef71d3cdd
André Gustavo <andregustavo147@gmail.com>
2025-07-24 16:55:04 -0300

    chore: use clients data table component

--- src/app/admin/clientes/page.tsx
+++ src/app/admin/clientes/page.tsx
@@ -1,24 +1,10 @@
-import { DeleteDropDownItem } from "../../admin/clientes/_components/UsersActions";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 import db from "@/lib/db";
-import { MoreVertical } from "lucide-react";
-import { formatCurrency, formatNumber } from "@/lib/formatters";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { AdminHeader } from "../_components/ui/admin-header";
 
-function getUsers() {
-  return db.user.findMany({
+import { AdminHeader } from "../_components/ui/admin-header";
+import { DashboardDataTable } from "../_components/DashboardDataTable";
+
+export default async function UsersPage() {
+  const clientsData = await db.user.findMany({
     select: {
       id: true,
       email: true,
@@ -26,62 +12,14 @@ function getUsers() {
     },
     orderBy: { createdAt: "desc" },
   });
-}
 
-export default function UsersPage() {
   return (
     <>
       <AdminHeader
         currentPage="Clientes"
         previousPage={[{ title: "Dashboard", url: "/admin" }]}
       />
-      <UsersTable />
+      <DashboardDataTable clientsData={clientsData} dataType="clients" />
     </>
-  );
-}
-
-async function UsersTable() {
-  const users = await getUsers();
-
-  if (users.length === 0) return <p>Nenhum cliente encontrado</p>;
-
-  return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Email</TableHead>
-          <TableHead>Pedidos</TableHead>
-          <TableHead>Valor</TableHead>
-          <TableHead className="w-0">
-            <span className="sr-only">Ações</span>
-          </TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {users.map((user) => (
-          <TableRow key={user.id}>
-            <TableCell>{user.email}</TableCell>
-            <TableCell>{formatNumber(user.orders.length)}</TableCell>
-            <TableCell>
-              {formatCurrency(
-                user.orders.reduce((sum, o) => o.pricePaidInCents + sum, 0) /
-                  100
-              )}
-            </TableCell>
-            <TableCell className="text-center">
-              <DropdownMenu>
-                <DropdownMenuTrigger>
-                  <MoreVertical className="cursor-pointer" />
-                  <span className="sr-only">Ações</span>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  <DeleteDropDownItem id={user.id} />
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
   );
 }
