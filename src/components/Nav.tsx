36b832d427c99c415fce1548b0082621b5d4a43e
André Gustavo <andregustavo147@gmail.com>
2025-08-04 22:55:58 -0300

    refactor: update nav component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,27 +1,113 @@
 "use client";
 
-import { cn } from "@/lib/utils";
+import * as React from "react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { ComponentProps, ReactNode } from "react";
 
-export function Nav({ children }: { children: ReactNode }) {
-  return (
-    <nav className="bg-primary text-primary-foreground flex justify-center px-4">
-      {children}
-    </nav>
-  );
-}
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { MenuIcon, ShoppingBag, UserRound } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Separator } from "./ui/separator";
 
-export function NavLink(props: Omit<ComponentProps<typeof Link>, "className">) {
-  const pathName = usePathname();
+export function Nav() {
   return (
-    <Link
-      {...props}
-      className={cn(
-        "p-6 hover:bg-secondary hover:text-secondary-foreground focus-visible:bg-secondary focus-visible:text-secondary-foreground",
-        pathName === props.href && "bg-background text-foreground"
-      )}
-    />
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4 container mx-auto">
+        {/* Menu telas pequenas */}
+        <div className="md:hidden">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant={"ghost"} size={"icon"}>
+                <MenuIcon className="h-6 w-6" />
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="w-60">
+              <SheetHeader>
+                <SheetTitle>Menu</SheetTitle>
+              </SheetHeader>
+              <div className="grid flex-1 auto-rows-min  *:data-[slot=link]:hover:bg-accent *:data-[slot=link]:px-3 *:data-[slot=link]:py-3">
+                <Link data-slot="link" href="/">
+                  Home
+                </Link>
+                <Separator />
+                <Link data-slot="link" href="/produtos">
+                  Produtos
+                </Link>
+                <Separator />
+                <Link data-slot="link" href="/pedidos">
+                  Meus Pedidos
+                </Link>
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
+
+        {/* Logo */}
+        <Link href="/" className="mr-6">
+          <h1 className="text-xl font-bold">PDF Books</h1>
+        </Link>
+
+        {/* Links de navegacao */}
+        <NavigationMenu className="hidden md:flex">
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuLink
+                asChild
+                className={navigationMenuTriggerStyle()}
+              >
+                <Link href="/">Home</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink
+                asChild
+                className={navigationMenuTriggerStyle()}
+              >
+                <Link href="/produtos">Produtos</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink
+                asChild
+                className={navigationMenuTriggerStyle()}
+              >
+                <Link href="/pedidos">Pedidos</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+
+        {/* Barra de pesquisa e icones de acao */}
+        <div className="ml-auto flex items-center space-x-4">
+          <Input
+            type="search"
+            placeholder="Pesquisar livros..."
+            className="w-full sm:w-[300px]"
+          />
+          <Button variant="ghost" size="icon">
+            <ShoppingBag className="h-5 w-5" />
+            <span className="sr-only">Carrinho de Compras</span>
+          </Button>
+          <Button variant={"ghost"} size={"icon"}>
+            <UserRound className="h-5 w-5" />
+            <span className="sr-only">Perfil do Usuário</span>
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 }
