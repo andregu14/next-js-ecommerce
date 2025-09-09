f43d59041db333871b24af4587dffda2bfa52bf3
André Gustavo <andregustavo147@gmail.com>
2025-09-08 23:02:33 -0300

    chore: create product details page

---
+++ src/app/(client)/produto/[id]/page.tsx
@@ -0,0 +1,69 @@
+import db from "@/lib/db";
+import { notFound } from "next/navigation";
+import Image from "next/image";
+import { formatCurrency } from "@/lib/formatters";
+import { Button } from "@/components/ui/button";
+
+type ProductsPageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function ProductPage({ params }: ProductsPageProps) {
+  const product = await db.product.findUnique({
+    where: { id: params.id },
+  });
+
+  if (!product) {
+    notFound();
+  }
+
+  const price = formatCurrency(product.priceInCents / 100);
+
+  return (
+    <div className="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8">
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:gap-12">
+        {/* Imagem */}
+        <div className="relative aspect-square overflow-hidden rounded-lg bg-muted">
+          <Image
+            src={product.imagePath}
+            alt={product.name}
+            fill
+            className="object-cover"
+            priority
+          />
+        </div>
+
+        {/* Detalhes */}
+        <div className="flex flex-col">
+          <h1 className="text-3xl font-bold tracking-tight lg:text-4xl">
+            {product.name}
+          </h1>
+
+          <p className="mt-4 text-3xl font font-semibold">{price}</p>
+
+          <div className="mt-6">
+            <h3 className="sr-only">Descrição</h3>
+            <div className="space-y-6 text-base text-muted-foreground">
+              <p>{product.description}</p>
+            </div>
+          </div>
+
+          <div className="mt-8 flex flex-col gap-4">
+            <Button size="lg">Adicionar ao Carrinho</Button>
+            <Button size="lg" variant={"outline"}>
+              Comprar Agora
+            </Button>
+          </div>
+
+          <div className="mt-auto pt-8">
+            <p className="text-sm text-muted-foreground">
+              Download imediato após a compra.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
